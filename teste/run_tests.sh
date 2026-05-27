43a062a69a82f083cdf56c8c8c1f8207efda42e7
Vinícius Landim <lviniciusaugusto@hotmail.com>
2026-05-27 20:01:04 -0300

    .sh para testes

---
+++ teste/run_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+./teste_processos 10 10000 1000000 > resultado1_mf_10.txt && \
+./teste_processos 50 10000 1000000 > resultado1_mf_50.txt && \
+./teste_processos 100 10000 1000000 > resultado1_mf_100.txt && \
+./teste_processos 200 10000 1000000 > resultado1_mf_200.txt && \
+./teste_processos 300 10000 1000000 > resultado1_mf_300.txt && \
+
+./teste_processos 10 10000 1000000 > resultado2_mf_10.txt && \
+./teste_processos 50 10000 1000000 > resultado2_mf_50.txt && \
+./teste_processos 100 10000 1000000 > resultado2_mf_100.txt && \
+./teste_processos 200 10000 1000000 > resultado2_mf_200.txt && \
+./teste_processos 300 10000 1000000 > resultado2_mf_300.txt && \
+
+./teste_processos 10 10000 1000000 > resultado3_mf_10.txt && \
+./teste_processos 50 10000 1000000 > resultado3_mf_50.txt && \
+./teste_processos 100 10000 1000000 > resultado3_mf_100.txt && \
+./teste_processos 200 10000 1000000 > resultado3_mf_200.txt && \
+./teste_processos 300 10000 1000000 > resultado3_mf_300.txt && \
+
+./teste_processos 10 10000 1000000 > resultado4_mf_10.txt && \
+./teste_processos 50 10000 1000000 > resultado4_mf_50.txt && \
+./teste_processos 100 10000 1000000 > resultado4_mf_100.txt && \
+./teste_processos 200 10000 1000000 > resultado4_mf_200.txt && \
+./teste_processos 300 10000 1000000 > resultado4_mf_300.txt && \
+
+./teste_processos 10 10000 1000000 > resultado5_mf_10.txt && \
+./teste_processos 50 10000 1000000 > resultado5_mf_50.txt && \
+./teste_processos 100 10000 1000000 > resultado5_mf_100.txt && \
+./teste_processos 200 10000 1000000 > resultado5_mf_200.txt && \
+./teste_processos 300 10000 1000000 > resultado5_mf_300.txt
