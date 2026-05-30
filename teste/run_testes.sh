323dde82facd1c7de5e1739692cdb6a2020f1c5d
nataliavitoriaamaral <natalia.amaraldoval@gmail.com>
2026-05-30 16:27:13 -0300

    Arquivo de testes unificados

---
+++ teste/run_testes.sh
@@ -0,0 +1,4 @@
+./teste_processos 10 1000000 100000000 > resultados_FCFS01.txt && \
+./teste_processos 50 1000000 100000000 > resultados_FCFS02.txt && \
+./teste_processos 100 1000000 100000000 > resultados_FCFS03.txt && \
+./teste_processos 200 1000000 100000000 > resultados_FCFS04.txt
