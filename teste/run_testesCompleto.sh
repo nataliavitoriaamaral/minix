e5b33e2c71517726ec3badcad8888f2e7a1568b0
nataliavitoriaamaral <natalia.amaraldoval@gmail.com>
2026-05-31 09:31:03 -0300

    Segundo arquivo de testes

---
+++ teste/run_testesCompleto.sh
@@ -0,0 +1,26 @@
+
+./teste_processos 10 10000 1000000 > resultados1FCFS05_10.txt && \
+./teste_processos 50 10000 1000000 > resultados1FCFS06_50.txt && \
+./teste_processos 100 10000 1000000 > resultados1FCFS07_100.txt && \
+./teste_processos 200 10000 1000000 > resultados1FCFS08_200.txt && \
+./teste_processos 300 10000 1000000 > resultados1FCFS09_300.txt && \
+./teste_processos 10 10000 1000000 > resultados2FCFS10_10.txt && \
+./teste_processos 50 10000 1000000 > resultados2FCFS11_50.txt && \
+./teste_processos 100 10000 1000000 > resultados2FCFS12_100.txt && \
+./teste_processos 200 10000 1000000 > resultados2FCFS13_200.txt && \
+./teste_processos 300 10000 1000000 > resultados2FCFS14_300.txt && \
+./teste_processos 10 10000 1000000 > resultados3FCFS15_10.txt && \
+./teste_processos 50 10000 1000000 > resultados3FCFS16_50.txt && \
+./teste_processos 100 10000 1000000 > resultados3FCFS17_100.txt && \
+./teste_processos 200 10000 1000000 > resultados3FCFS18_200.txt && \
+./teste_processos 300 10000 1000000 > resultados3FCFS19_300.txt && \
+./teste_processos 10 10000 1000000 > resultados4FCFS20_10.txt && \
+./teste_processos 50 10000 1000000 > resultados4FCFS21_50.txt && \
+./teste_processos 100 10000 1000000 > resultados4FCFS22_100.txt && \
+./teste_processos 200 10000 1000000 > resultados4FCFS23_200.txt && \
+./teste_processos 300 10000 1000000 > resultados4FCFS24_300.txt && \
+./teste_processos 10 10000 1000000 > resultados5FCFS25_10.txt && \
+./teste_processos 50 10000 1000000 > resultados5FCFS26_50.txt && \
+./teste_processos 100 10000 1000000 > resultados5FCFS27_100.txt && \
+./teste_processos 200 10000 1000000 > resultados5FCFS28_200.txt && \
+./teste_processos 300 10000 1000000 > resultados5FCFS29_300.txt
