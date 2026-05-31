cd713738f0b42b9712f4157c4792cbb7b35b79b9
~gbonavina~ <~gabriellbonavina@gmail.com~>
2026-05-31 22:32:47 +0000

    new tests on rr

---
+++ teste/testes.sh
@@ -0,0 +1,4 @@
+./teste_processos 10 1000000 100000000 > resultados_rr_10.txt && \
+./teste_processos 50 1000000 100000000 > resultados_rr_50.txt && \
+./teste_processos 100 1000000 100000000 > resultados_rr_100.txt && \
+./teste_processos 200 1000000 100000000 > resultados_rr_200.txt 
