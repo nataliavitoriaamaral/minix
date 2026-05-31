bcec75c83b60c4aa0540b896b261dff39fffe7b8
Vinícius Landim <lviniciusaugusto@hotmail.com>
2026-05-31 09:09:07 -0300

    .sh de testes de parametros 1_000_000 100_000_000

---
+++ teste/tests_johannes.sh
@@ -0,0 +1,5 @@
+./teste_processos 10 1000000 100000000 > resultados_mf_10.txt && \
+./teste_processos 50 1000000 100000000 > resultados_mf_50.txt && \
+./teste_processos 100 1000000 100000000 > resultados_mf_100.txt && \
+./teste_processos 200 1000000 100000000 > resultados_mf_200.txt && \
+./teste_processos 300 1000000 100000000 > resultados_mf_300.txt
