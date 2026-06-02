bdb41661cc68e1ee0888cd31c68084051b9581c0
JollonLD <jonas.durao@unifesp.br>
2026-06-02 22:22:41 +0000

    testes minix padrao

--- teste/teste_johannes.sh
+++ teste/teste_johannes.sh
@@ -1,4 +1,3 @@
-./teste_processos 10 1000000 100000000 > resultados_bl_j_10.txt && \
-./teste_processos 50 1000000 100000000 > resultados_bl_j_50.txt && \
+
 ./teste_processos 100 1000000 100000000 > resultados_bl_j_100.txt && \
 ./teste_processos 200 1000000 100000000 > resultados_bl_j_200.txt && \
