73845a98250294effcbde691f8a28de15bef5d89
Vinícius Landim <lviniciusaugusto@hotmail.com>
2026-06-03 21:58:31 -0300

    removido teste de 300 processos em run_tests.sh

--- teste/run_tests.sh
+++ teste/run_tests.sh
@@ -4,24 +4,19 @@
 ./teste_processos 50 100000 10000000 > resultado1_mf_50.txt && \
 ./teste_processos 100 100000 10000000 > resultado1_mf_100.txt && \
 ./teste_processos 200 100000 10000000 > resultado1_mf_200.txt && \
-./teste_processos 300 100000 10000000 > resultado1_mf_300.txt && \
 ./teste_processos 10 100000 10000000 > resultado2_mf_10.txt && \
 ./teste_processos 50 100000 10000000 > resultado2_mf_50.txt && \
 ./teste_processos 100 100000 10000000 > resultado2_mf_100.txt && \
 ./teste_processos 200 100000 10000000 > resultado2_mf_200.txt && \
-./teste_processos 300 100000 10000000 > resultado2_mf_300.txt && \
 ./teste_processos 10 100000 10000000 > resultado3_mf_10.txt && \
 ./teste_processos 50 100000 10000000 > resultado3_mf_50.txt && \
 ./teste_processos 100 100000 10000000 > resultado3_mf_100.txt && \
 ./teste_processos 200 100000 10000000 > resultado3_mf_200.txt && \
-./teste_processos 300 100000 10000000 > resultado3_mf_300.txt && \
 ./teste_processos 10 100000 10000000 > resultado4_mf_10.txt && \
 ./teste_processos 50 100000 10000000 > resultado4_mf_50.txt && \
 ./teste_processos 100 100000 10000000 > resultado4_mf_100.txt && \
 ./teste_processos 200 100000 10000000 > resultado4_mf_200.txt && \
-./teste_processos 300 100000 10000000 > resultado4_mf_300.txt && \
 ./teste_processos 10 100000 10000000 > resultado5_mf_10.txt && \
 ./teste_processos 50 100000 10000000 > resultado5_mf_50.txt && \
 ./teste_processos 100 100000 10000000 > resultado5_mf_100.txt && \
-./teste_processos 200 100000 10000000 > resultado5_mf_200.txt && \
-./teste_processos 300 100000 10000000 > resultado5_mf_300.txt
+./teste_processos 200 100000 10000000 > resultado5_mf_200.txt
